156914c825660f37102048bdafc96c928dad2a48
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-08 22:49:58 -0600

    Had reasons table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309004246) do
+ActiveRecord::Schema.define(version: 20150309030605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20150309004246) do
     t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reasons", force: :cascade do |t|
+    t.string  "name"
+    t.integer "priority"
+    t.integer "life_venture_id"
   end
 
   create_table "users", force: :cascade do |t|
