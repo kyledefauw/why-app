13ac63cc876a3d5e107239f0561a5357f99e14cd
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-08 22:50:43 -0600

    Finish Sign In pages

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,11 +5,20 @@ class AuthenticationController < ApplicationController
   end
 
   def create
-    user = User.new(email: params[:email])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate[:password]
       session[:user_id] = user.id
       flash[:notice] = 'User successfully created!'
       redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    redirect_to root_path, notice: 'You have successfully logged out'
   end
 
 end
