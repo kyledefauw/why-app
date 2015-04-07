f8784fb04b0218693cdf12eb80fa3b35570ae7e5
Kyle DeFauw <kyle.defauw@gmail.com>
2015-04-07 15:40:02 -0600

    Fix authentication sign in

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,23 +1,23 @@
 class AuthenticationController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate[:password]
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = 'User successfully created!'
+      flash[:notice] = 'Login Successful'
       redirect_to '/'
     else
+      flash[:notice] = 'Email / Password combination wrong'
       render :new
     end
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.delete
+    session[:user_id] = nil
     redirect_to root_path, notice: 'You have successfully logged out'
   end
 
